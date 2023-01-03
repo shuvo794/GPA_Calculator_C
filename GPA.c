f3381f196fdbd5a3da4c520b7dac084b920516a8
shuvo794 <shuvohossain794@gmail.com>
2023-01-03 11:36:25 +0600

    write conditione

--- GPA.c
+++ GPA.c
@@ -4,6 +4,34 @@ int main(){
     int marks,gread_point;
     scanf("%d",&marks);
 
+    if(marks>=80){
+        printf(" GPA  A+\n");
+        gread_point=5;
+       
+    }
+    else if (marks>=70)
+    {
+        printf(" GPA  A\n");
+        gread_point=4;
+    }
+    else if (marks>=60)
+    {
+        printf(" GPA  A-\n");
+        gread_point=3.5;
+    }
+    else if (marks>=50)
+    {
+        printf(" GPA  B\n");
+        gread_point=3;
+    }
+    else if (marks>=40)
+    {
+        printf(" GPA  C\n");
+        gread_point=2;
+    }
+    else{
+        printf("F");
+    }
 
 
 
